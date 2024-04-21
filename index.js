f094a3a023605e27adb30e1e5524b0662cacf37a
QingjieShen <122641381+QingjieShen@users.noreply.github.com>
2024-04-21 17:40:26 -0400

    adds auto copy hex function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const color = document.getElementById('color-picker')
 const mode = document.getElementById('mode')
 const colorsContainer = document.querySelector('.colors')
 const getBtn = document.getElementById('getBtn')
+const copyText = document.getElementById('copy')
 
 const baseUrl = 'https://www.thecolorapi.com/scheme'
 
@@ -25,10 +26,23 @@ function setColor(colors) {
     const colorsEle = []
     colors.map(color => {
         colorsEle.push(`
-            <div class="color" style="background-color:${color.hex.value};">
+            <div data-hex=${color.hex.value} class="color" style="background-color:${color.hex.value};">
                 <p class="hex-text">${color.hex.value}</p>
             </div>
         `)
     })
     colorsContainer.innerHTML = colorsEle.join('')
+    copyText.textContent = "Click color to copy hex code"
 }
+
+colorsContainer.addEventListener('click', (e) => {
+    console.log(e.target)
+    if (e.target.classList.contains('color')) {
+        navigator.clipboard.writeText(e.target.dataset.hex)
+        copyText.textContent = `${e.target.dataset.hex} copied`
+    }
+    if (e.target.classList.contains('hex-text')) {
+        navigator.clipboard.writeText(e.target.parentElement.dataset.hex)
+        copyText.textContent = `${e.target.parentElement.dataset.hex} copied`
+    }
+})
